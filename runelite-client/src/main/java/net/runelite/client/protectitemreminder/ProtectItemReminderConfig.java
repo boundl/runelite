b99ba92cc0a894b67397c998caa946d12118c341
ben93riggs <42555892+ben93riggs@users.noreply.github.com>
2018-11-26 11:21:53 -0600

    changes

---
+++ runelite-client/src/main/java/net/runelite/client/protectitemreminder/ProtectItemReminderConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.protectitemreminder;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("protectitemreminder")
+public interface ProtectItemReminderConfig extends Config
+{
+
+}
